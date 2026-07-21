124dc3431b66c4a938456e9f7bf068538998cc4e
Rupsa Roy <rupsaroy305@gmail.com>
2026-07-21 19:18:29 +0530

    Time: 739 ms (26.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ Basic_Joins/3891-find-category-recommendation-pairs/find-category-recommendation-pairs.sql
@@ -0,0 +1,1 @@
+WITH c AS (SELECT DISTINCT user_id, category FROM ProductPurchases JOIN ProductInfo USING (product_id))SELECT c1.category AS category1, c2.category AS category2, COUNT(*) AS customer_count FROM c c1 JOIN c c2 ON c1.user_id = c2.user_id AND c1.category < c2.category GROUP BY category1, category2 HAVING customer_count >= 3 ORDER BY customer_count DESC, category1, category2;
