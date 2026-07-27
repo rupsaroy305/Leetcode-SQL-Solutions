8bb42a24bbbf1acd38dd2561e9def606e7e4010d
Rupsa Roy <rupsaroy305@gmail.com>
2026-07-27 11:45:05 +0530

    Time: 287 ms (79.26%) | Memory: 0B (100.00%) - LeetSync

---
+++ advanced-string-functions-regex-clause/3816-dna-pattern-recognition/dna-pattern-recognition.sql
@@ -0,0 +1,1 @@
+SELECT sample_id,dna_sequence,species,dna_sequence REGEXP '^ATG' has_start,dna_sequence REGEXP '(TAA|TAG|TGA)$' has_stop,dna_sequence LIKE '%ATAT%' has_atat,dna_sequence REGEXP 'GGG+' has_ggg FROM Samples ORDER BY sample_id;
