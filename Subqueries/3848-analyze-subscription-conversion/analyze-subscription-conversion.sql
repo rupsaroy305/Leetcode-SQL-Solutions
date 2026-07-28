2ad18b99a5e8aff115e8f81709b119d8b8a496ff
Rupsa Roy <rupsaroy305@gmail.com>
2026-07-28 13:01:46 +0530

    Time: 680 ms (5.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ Subqueries/3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -0,0 +1,7 @@
+SELECT user_id,
+ROUND(AVG(CASE WHEN activity_type='free_trial' THEN activity_duration END),2) trial_avg_duration,
+ROUND(AVG(CASE WHEN activity_type='paid' THEN activity_duration END),2) paid_avg_duration
+FROM UserActivity
+GROUP BY user_id
+HAVING SUM(CASE WHEN activity_type='free_trial' THEN 1 ELSE 0 END)>0
+   AND SUM(CASE WHEN activity_type='paid' THEN 1 ELSE 0 END)>0;
